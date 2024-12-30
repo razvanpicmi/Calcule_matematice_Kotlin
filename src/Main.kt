2800da083e9665bc80b7743b160574cf0d168a5a
razvanpicmi <47541060+razvanpicmi@users.noreply.github.com>
2024-12-30 23:08:40 +0100

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val lungime: Double = 5.0
+    val latime: Double = 3.0
+    val arie: Double = lungime * latime
+
+    println("Lungime: $lungime, Lățime: $latime, Arie: $arie")
+}
